9156ddf0e3ca8ce7cedc19b28d7fb58f033b3e7c
jeremyelkayam <jeremyelkayam@gmail.com>
2015-05-21 18:30:50 -0400

    Started work on the Cell class

--- Cell.java
+++ Cell.java
@@ -1,4 +1,13 @@
-public class Cell{
-    
-    
+/*What does a cell need?
+  - Must store any type of data that you can enter into the spreadsheet
+  
+ */
+public class Cell<T>{
+    T data;
+    public Cell(T d){
+	data=d;
+    }
+    public T getData(){
+	return data;
+    }
 }
