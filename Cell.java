97273e14d971697b12409b47ca8748547552f61d
jeremyelkayam <jeremyelkayam@gmail.com>
2015-05-21 21:39:43 -0400

    Worked on the cell class. Added a method to check whether or not it has a number & also methods to get/set data.

--- Cell.java
+++ Cell.java
@@ -1,14 +1,43 @@
 /*What does a cell need?
   - Must store any type of data that you can enter into the spreadsheet
-  - 
+  - Should be able to do operations that reference other cells (maybe make a MathCell class that extends Cell?)
+  - MathCell could have a reference to its sheet so that it can easily check other cells
   
  */
-public class Cell<T>{
-    T data;
-    public Cell(T d){
+public class Cell{
+    double data;//store all numbers internally as doubles since it can only store numbers and strings
+    boolean containsNumber;//whether or not we have a number
+    String str; //data in string form
+    public Cell(double d){
 	data=d;
+	str=String.valueOf(d);
+	containsNumber=true;
     }
-    public T getData(){
-	return data;
+    public Cell(String s){
+	str=s;
+	containsNumber=false;
     }
+    public double getData() throws UnsupportedOperationException{
+	if(containsNumber){
+	    return data;
+	}else{
+	    throw new UnsupportedOperationException("This cell contains a string");
+	}
+    }
+    public String toString(){
+	return str;
+    }
+    public boolean containsNumber(){
+	return containsNumber;
+    }
+    public void setData(double d){
+	data=d;
+	str=String.valueOf(d);
+	containsNumber=true;
+    }
+    public void setData(String s){
+	str=s;
+	containsNumber=false;
+    }
+    
 }
