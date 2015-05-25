def50e9c1af73c46d220a0d16dadcf8f1e8e155e
jeremyelkayam <jeremyelkayam@gmail.com>
2015-05-24 22:51:52 -0400

    Still trying out stuff. Interface looks a little like a spreadsheet now. Still have no idea what I'm doing

--- Interface.java
+++ Interface.java
@@ -7,19 +7,23 @@ import java.awt.event.*;
 public class Interface extends JFrame{
     private Container pane;
     private JTextField text;
+    private String title;
     public Interface(){
-	this.setTitle("Spreadsheet");
-	this.setSize(600,500);
+	title="Stuycrosoft XL";
+	this.setTitle(title);
+	this.setSize(800,600);
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	pane=this.getContentPane();
-	pane.setLayout(new GridLayout(5,5));
+	pane.setLayout(new GridLayout(20,5));
 	text = new JTextField();
-	//Feel free to delete the following code, it's just me testing out stuff
+	//Feel free to delete the following code, it's just me trying out stuff
 	pane.add(text);
 	pane.add(new JTextField());
 	pane.add(new JTextField());
-	
+	for(int z=0;z<300;z++){
+	    pane.add(new JTextField());
+	}
     }
     public static void main(String[]args){
 	Interface blah = new Interface();
