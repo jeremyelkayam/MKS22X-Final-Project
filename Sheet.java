3ab24f7c423094364ec56e371fb3c6cef2c85744
jeremyelkayam <jeremyelkayam@gmail.com>
2015-05-25 17:38:10 -0400

    GetCell method in Sheet class

--- Sheet.java
+++ Sheet.java
@@ -3,5 +3,14 @@ public class Sheet{
     public Sheet(){
 	sheet=new Cell[50][50];
     }
-    
+    public Cell getCell(int row, int col){
+	return cell[row][col];
+    }
+    public void enlarge(boolean sideways){
+	if(sideways){//expand sideways
+	    
+	}else{//expand vertically
+
+	}
+    }
 }
