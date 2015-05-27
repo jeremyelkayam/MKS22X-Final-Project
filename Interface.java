cb77ba3d8355ee7be3082a2c2faa1aaed1415fc7
Tina Zhang <tinazhang1197@gmail.com>
2015-05-27 11:58:02 -0400

    added save option to file menu

--- Interface.java
+++ Interface.java
@@ -28,23 +28,89 @@ public class Interface extends JFrame{
 
 	//adding menu bar
 	JMenuBar menuBar;
-	JMenu FileMenu;
+	JMenu fileMenu, editMenu;
 	JMenuItem menuItem;
 
 	//create the menu bar
 	menuBar = new JMenuBar();
 
 	//build the first menu
-	FileMenu = new JMenu("File");
-	//open File menu with crtl + f
-	FileMenu.setMnemonic('F');
-	menuBar.add(FileMenu);
+	fileMenu = new JMenu("File");
+	menuBar.add(fileMenu);
+
+	editMenu = new JMenu("Edit");
+	menuBar.add(editMenu);
 	
 	//adding items to FileMenu
 	menuItem = new JMenuItem("New");
+	menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.ALT_MASK));
 	menuItem.getAccessibleContext().setAccessibleDescription("Open a new file");
-	FileMenu.add(menuItem);
+	fileMenu.add(menuItem);
+
+	menuItem = new JMenuItem("Open..");
+	menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.ALT_MASK));
+	menuItem.getAccessibleContext().setAccessibleDescription("Open existing file");
+	fileMenu.add(menuItem);
+
+	menuItem = new JMenuItem("Rename..");
+	menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, ActionEvent.ALT_MASK));
+	menuItem.getAccessibleContext().setAccessibleDescription("Rename file");
+	fileMenu.add(menuItem);
+
+	menuItem = new JMenuItem("Save");
+	menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.ALT_MASK));
+	menuItem.getAccessibleContext().setAccessibleDescription("Save file");
+	fileMenu.add(menuItem);
+
+	//adding items to editMenu
+	ImageIcon undo = new ImageIcon("images/undo.png");
+	Image undoScaled = undo.getImage().getScaledInstance(16,16,Image.SCALE_DEFAULT);
+	ImageIcon undoIcon = new ImageIcon(undoScaled);
+	menuItem = new JMenuItem("Undo", undoIcon);
+	menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, ActionEvent.ALT_MASK));
+	menuItem.getAccessibleContext().setAccessibleDescription("Undo");
+	editMenu.add(menuItem);
+
+	ImageIcon redo = new ImageIcon("images/redo.png");
+	Image redoScaled = redo.getImage().getScaledInstance(16,16,Image.SCALE_DEFAULT);
+	ImageIcon redoIcon = new ImageIcon(redoScaled);
+	menuItem = new JMenuItem("Redo", redoIcon);
+	menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, ActionEvent.ALT_MASK));
+	menuItem.getAccessibleContext().setAccessibleDescription("Redo");
+	editMenu.add(menuItem);
 	
+	//add line seperator
+	editMenu.addSeparator();
+
+	ImageIcon cut = new ImageIcon("images/cut.png");
+	Image cutScaled = cut.getImage().getScaledInstance(16,16,Image.SCALE_DEFAULT);
+	ImageIcon cutIcon = new ImageIcon(cutScaled);
+	menuItem = new JMenuItem("Cut", cutIcon);
+	menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.ALT_MASK));
+	menuItem.getAccessibleContext().setAccessibleDescription("Cut");
+	editMenu.add(menuItem);
+	
+	ImageIcon copy = new ImageIcon("images/copy.png");
+	Image copyScaled = copy.getImage().getScaledInstance(16,16,Image.SCALE_DEFAULT);
+	ImageIcon copyIcon = new ImageIcon(copyScaled);
+	menuItem = new JMenuItem("Copy", copyIcon);
+	menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.ALT_MASK));
+	menuItem.getAccessibleContext().setAccessibleDescription("Copy");
+	editMenu.add(menuItem);
+
+	ImageIcon paste = new ImageIcon("images/paste.png");
+	Image pasteScaled = paste.getImage().getScaledInstance(16,16,Image.SCALE_DEFAULT);
+	ImageIcon pasteIcon = new ImageIcon(pasteScaled);
+	menuItem = new JMenuItem("Paste", pasteIcon);
+	menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.ALT_MASK));
+	menuItem.getAccessibleContext().setAccessibleDescription("Paste");
+	editMenu.add(menuItem);
+
+	menuItem = new JMenuItem("Find..");
+	menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, ActionEvent.ALT_MASK));
+	menuItem.getAccessibleContext().setAccessibleDescription("Find..");
+	editMenu.add(menuItem);
+
 	setJMenuBar(menuBar);
     }
     public static void main(String[]args){
